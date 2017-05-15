486af98de48516e03eb06f29dd70aa2d66899897
Deadspeck <thaone344@yahoo.com>
2017-05-15 12:32:10 -0600

    new gui works

--- src/gitsupreme15may/DaMain.java
+++ src/gitsupreme15may/DaMain.java
@@ -5,7 +5,7 @@ public class DaMain {
 	public static void main(String[] args) {
 		System.out.println("initial build");
 		System.out.println("made a new gui");
-
+		System.out.println("new gui works");
 	}
 
 }

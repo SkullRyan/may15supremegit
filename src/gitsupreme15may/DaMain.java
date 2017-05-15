31c103dbd6e5ce24b5f27b1b0a4b1ab4b0a00d6f
Deadspeck <thaone344@yahoo.com>
2017-05-15 12:28:34 -0600

    bugfixed

--- src/gitsupreme15may/DaMain.java
+++ src/gitsupreme15may/DaMain.java
@@ -4,7 +4,8 @@ public class DaMain {
 
 	public static void main(String[] args) {
 		System.out.println("initial build");
-
+		System.out.println("working on a bug");
+		System.out.println("fixed");
 	}
 
 }

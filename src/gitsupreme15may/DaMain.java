a57f174936503e153ea92ab850c632a87beeaaad
Deadspeck <thaone344@yahoo.com>
2017-05-15 12:36:49 -0600

    Merge branch 'newgui'
    Conflicts:
    	src/gitsupreme15may/DaMain.java

--- src/gitsupreme15may/DaMain.java
+++ src/gitsupreme15may/DaMain.java
@@ -4,6 +4,8 @@ public class DaMain {
 
 	public static void main(String[] args) {
 		System.out.println("initial build");
+		
+		
 		System.out.println("made a new gui");
 		System.out.println("new gui works");
 	}

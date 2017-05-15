b2c3d2c473bb9643bdb76780014095ca26107478
Deadspeck <thaone344@yahoo.com>
2017-05-15 12:40:01 -0600

    century

--- src/gitsupreme15may/DaMain.java
+++ src/gitsupreme15may/DaMain.java
@@ -5,11 +5,7 @@ public class DaMain {
 	public static void main(String[] args) {
 		
 		System.out.println("initial build");
-		
-		
-		System.out.println("made a new gui");
-		System.out.println("new gui works");
-		
+	
 		System.out.println("THIS WILL GET ME A CENTURY!");
 	}
 
